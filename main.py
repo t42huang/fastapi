f7fac7479d3704573bad8bd3361825b1b5674ec1
tina <tina.huanght@shopee.com>
2024-04-19 15:27:31 +0800

    添加两个BaseModel请求体

--- main.py
+++ main.py
@@ -39,15 +39,18 @@ async def get_food(food_name: FoodEnum):
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "bar"}, {"item_name": "Baz"}, {"item_name": "TGIF"}, ]
 
+
 # Enum
 @app.get("/items")
 async def list_items(skip: int = 0, limit: int = 10):
     return fake_items_db[skip: skip + limit]
 
+
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
@@ -61,10 +64,11 @@ async def get_model(model_name: ModelName):
 
 # q 是可选参数，默认值位None
 @app.get("/items/2/{item_id}")
-async def read_item(item_id:str, q:Union[str, None] = None):
+async def read_item(item_id: str, q: Union[str, None] = None):
     if q:
-        return {"Item_id": item_id, "q":q}
+        return {"Item_id": item_id, "q": q}
     return {"item_id": id}
+
 
 # item_id 是路径参数， q是查询参数
 @app.get("/items/{item_id}")
@@ -72,6 +76,7 @@ async def list_items(item_id: str, q: Union[str, None] = Query(default=None, max
     if q:
         return {"item_id": item_id, "q": q}
     return {"item_id": item_id}
+
 
 # 多个路径和查询参数
 @app.get("/users/{user_id}/items/{item_id}")
@@ -87,8 +92,9 @@ async def get_user_item(user_id: int, item_id: str, q: Union[str, None], short: 
         )
         return item
 
+
 @app.get("/items/3/{item_id}")
-async def read_user_item(item_id: str, needy: str, skip: int=0, limit: Union[int, None] = None):
+async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None):
     item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
     return item
 
@@ -99,6 +105,12 @@ class Item(BaseModel):
     description: str
     price: float
     tax: Union[float, None] = None  # optional param
+
+
+class User(BaseModel):
+    username: str
+    full_name: Union[str, None] = None
+
 
 @app.post("/items")
 async def create_item(item: Item):
@@ -122,10 +134,11 @@ async def update_item(
         item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
         q: Union[str, None] = None,
         item: Union[Item, None] = None,
+        user: Union[User, None] = None
 ):
-    results = {"item_id": item_id}
+    results = {"item_id": item_id, "user": User}
     if q:
         results.update({"q": q})
     if item:
-        results.update({"item": item})
+        results = {"item": item, "user": User}
     return results
